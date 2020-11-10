d31e9f780d3d8eb2d7732705339ac19f28a8b69e
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-10 12:46:25 +0100

    Refactored using resources in routes and before_action in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'tasks' => 'tasks#index'
-  post 'tasks' => 'tasks#create'
-  get 'tasks/new' => 'tasks#new', as: :create_task
-  get 'tasks/:id' => 'tasks#show', as: :task
-  get 'tasks/:id/edit' => 'tasks#edit', as: :edit_task
-  patch 'tasks/:id' => 'tasks#update'
-  delete 'tasks/:id' => 'tasks#destroy', as: :delete_task
+  resources :tasks
 end
