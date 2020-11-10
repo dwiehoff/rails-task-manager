e77dcfea3a9e1b302a27995a35201f0a9b4271e9
Daniel Wiehoff <daniel.wiehoff+github@gmail.com>
2020-11-10 12:09:57 +0100

    Completed #new and #create to allow user to add new task

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'tasks' => 'tasks#index'
+  post 'tasks' => 'tasks#create'
+  get 'tasks/new' => 'tasks#new', as: :create_task
   get 'tasks/:id' => 'tasks#show', as: :task
 end
